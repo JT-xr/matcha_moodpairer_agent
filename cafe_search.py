7a5e372c3c3a968316a0f16f826b9df259ee117c
JT <james4business@hotmail.com>
2025-06-25 11:23:19 -0400

    Setup environment, created main file, created test file

---
+++ cafe_search.py
@@ -0,0 +1,31 @@
+# cafe_search.py
+
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+def search_matcha_cafes(location: str, radius=3000):
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+    params = {
+        "query": f"matcha cafe near {location}",
+        "radius": radius,
+        "key": GOOGLE_API_KEY,
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    cafes = []
+    for place in data.get("results", []):
+        cafes.append({
+            "name": place.get("name"),
+            "address": place.get("formatted_address"),
+            "rating": place.get("rating"),
+            "map_link": f"https://maps.google.com/?q={place.get('name').replace(' ', '+')}",
+        })
+
+    return cafes
